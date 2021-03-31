d9a974ccbbce20e34da84ca9ec2a06a5e9f42fc7
SilvioJavorek <silvio.javorek@vuv.hr>
2021-03-31 17:19:13 +0200

    treci commit

--- js/app.js
+++ js/app.js
@@ -7,11 +7,13 @@ var table=document.getElementById("tablicaPodataka");
   var cell2 = row.insertCell(1);
   var cell3 = row.insertCell(2);
   var cell4= row.insertCell(3);
+  var cell5= row.insertCell(4);
   
 cell1.innerHTML = document.getElementById("lPredmet").value;
 cell2.innerHTML = document.getElementById("lsemestar").value;
 cell3.innerHTML = document.getElementById("lEctsbodovi").value;
 cell4.innerHTML = '<button onclick="ObrisiRed(this)" class="fas fa-minus"></button> ';
+cell5.innerHTML = '<input type="checkbox" id="myCheck" onclick="selectedRow()">';
 
 }
 
@@ -20,4 +22,20 @@ function ObrisiRed(rows)
 {
 var _row = rows.parentElement.parentElement;
   document.getElementById('tablicaPodataka').deleteRow(_row.rowIndex);
+}
+
+
+function selectedRow()
+{
+  var table = document.getElementById('tablicaPodataka');
+
+  table.addEventListener("click", function(ev){
+    if (ev.target.tagName == "INPUT") {
+      if (ev.target.checked) {
+        ev.target.parentNode.parentNode.classList.add("selected");
+      }else {
+        ev.target.parentNode.parentNode.classList.remove("selected");
+      }
+    }
+  });
 }
