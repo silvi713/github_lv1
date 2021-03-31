92562af9d28bf2ce6dd871e724f1a52f92922873
SilvioJavorek <silvio.javorek@vuv.hr>
2021-03-31 17:15:24 +0200

    drugi commit

--- js/app.js
+++ js/app.js
@@ -6,9 +6,18 @@ var table=document.getElementById("tablicaPodataka");
   var cell1 = row.insertCell(0);
   var cell2 = row.insertCell(1);
   var cell3 = row.insertCell(2);
+  var cell4= row.insertCell(3);
   
 cell1.innerHTML = document.getElementById("lPredmet").value;
 cell2.innerHTML = document.getElementById("lsemestar").value;
 cell3.innerHTML = document.getElementById("lEctsbodovi").value;
+cell4.innerHTML = '<button onclick="ObrisiRed(this)" class="fas fa-minus"></button> ';
 
+}
+
+
+function ObrisiRed(rows)
+{
+var _row = rows.parentElement.parentElement;
+  document.getElementById('tablicaPodataka').deleteRow(_row.rowIndex);
 }
